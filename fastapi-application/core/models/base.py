5e2ddae28a7b6643731ad9d4556e57472f6442f1
Alexander Malshakov <89681115653@mail.ru>
2025-02-04 10:16:25 +0300

    add naming_convention

--- fastapi-application/core/models/base.py
+++ fastapi-application/core/models/base.py
@@ -1,3 +1,4 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import (
     DeclarativeBase,
     declared_attr,
@@ -5,11 +6,14 @@ from sqlalchemy.orm import (
     mapped_column,
 )
 
+from core.config import settings
 from utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     def __tablename__(cls) -> str:
